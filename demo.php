8ae4f8f9e10bc2e91e185e46dfb53d0a4302ff2c
Florin Patan <florinpatan@gmail.com>
2012-09-19 09:39:15 +0300

    More demo work

--- demo.php
+++ demo.php
@@ -2,14 +2,32 @@
 
 include 'solr.php';
 
-$solrQuery = new Solr;
+class SolrDemo {
 
-$solrQuery
-    ->setConnection('valid_dsn_here')
-    ->setQueryFields(array('nume', 'categorie', 'pret', 'rating'))
-    ->setOrderFields(array('relevance' => 'desc', 'pret' => 'asc', 'nume' => 'asc'))
-    ->setReturnFields(array('id'))
-    ->setQuery('tastatura MS');
+    private $solr = null;
 
-var_dump($solrQuery->getResults());
+    public function __construct(Solr $solr) {
+        $this->solr = $solr
+            ->setConnection('valid_dsn_here')
+            ->setQueryFields(array('nume', 'categorie', 'pret', 'rating'))
+            ->setOrderFields(array('relevance' => 'desc', 'pret' => 'asc', 'nume' => 'asc'))
+            ->setReturnFields(array('id'));
+    }
+
+    public function query($query, $page = 0, $resultsPerPage = 0) {
+        return $this->solr
+            ->setQuery($query)
+            ->getResults($page, $resultsPerPage);
+    }
+
+}
+
+$solrQuery = new SolrDemo(new Solr());
+
+var_dump($solrQuery->query('tastatura MS'));
+
+
+
+// id, name, description, short_description, price, rating, category_id, subcategory_id,
+// normalized_name, normalized_description, normalized_short_description, normalized_category, normalized_subcategory
 
--- emag.solr.php
+++ emag.solr.php
@@ -28,6 +28,7 @@ echo "\n\n";
 //query:"{!dismax qf=searchText bq='(: -siteId:1 -siteId:2 -siteId:16)^1.5 OR canEmbed:1^1.005'}lesbian"
 
 $search = "http://dev-sk.freeporn.com/florin/solrbj.php?q={!boost+b%3Drecip%28ms%28NOW%2CdateAdded%29%2C3.16e-1%2C1%2C1%29}canEmbed%3A%280^0.1+OR+1^1000%29+AND+searchText%3A%28eva+and+angelina%29&start=0&rows=100";
+//        'http://dev-sk.freeporn.com/florin/solrbj.php?q={!boost b=recip(ms(NOW,dateAdded),3.16e-1,1,1)} canEmbed:(0^0.1 OR 1^1000) AND searchText:(eva and angelina)&start=0&rows=100';
 parse_str($search,  $searchArr);
 echo '<pre>';
 print_r($searchArr);
